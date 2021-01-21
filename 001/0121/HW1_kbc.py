036d26a9176614a3dac6b2be587684b0d7b69f9a
bottlesteel97 <41908146+BottleChrome@users.noreply.github.com>
2021-01-21 17:36:28 +0900

    Test

--- 001/0121/HW1_kbc.py
+++ 001/0121/HW1_kbc.py
@@ -35,5 +35,4 @@ count = 0
 for i in range(1,n+1):
     if i < sumYaksu(i)-i:
         count += 1
-        print(i)
 print(count)
--- 001/0121/Lab2_6.py
+++ 001/0121/Lab2_6.py
@@ -1,16 +1,32 @@
 
 
-answer = 0 
-yaksu_list = []
+def sumYaksu(n):
+    s1 = 1
+    if n % 2 == 0:
+        s = 1 
+        while n%2 == 0:
+            s = s*2+1
+            n //= 2
+        s1 *= s
+    i = 3 
+    while i*i <= n :
+        if n%i != 0 :
+            i+=2
+            continue
+        s = 1
+        while n%i == 0:
+            s = s*i+1
+            n//=i
+        s1 *= s
+        i+=2
+    if n > 1 :
+        s1 *= n+1
+    return s1
 
+n= int(input())
 count = 0
-for i in range(1,int(math.sqrt(n))+1):
-    if i * i == n : 
+for i in range(1,n+1):
+    if i == sumYaksu(i)-i:
+        print(i)
         count += 1
-        yaksu_list.append(i)
-    elif n % i == 0 :
-        count += 2
-        yaksu_list.append(i)
-        yaksu_list.append(int(n/i))
-
-print(sum(yaksu_list))
+print(count)
