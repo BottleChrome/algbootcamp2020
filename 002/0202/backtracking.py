8da25f6d7bf878f8546e8e2fdc668c52cb46f8dc
bottlesteel97 <41908146+BottleChrome@users.noreply.github.com>
2021-02-02 13:15:25 +0900

    Create backtracking.py

---
+++ 002/0202/backtracking.py
@@ -0,0 +1,21 @@
+def backtracking(varr, current, n, k) :
+  if current == k :
+    s = " "
+    for i in range(k):
+      s += str(varr[i]) + ' '
+    print(s)
+    return
+
+  for i in range(1, n+1):
+    isOK = True
+    for j in range(current) :
+      if i == varr[j] :
+        isOK = False
+        break
+    if not isOK : continue
+    varr[current] = i
+    backtracking(varr, current+1, n, k )
+
+n , k = map(int, input().split())
+varr = [0] * k
+backtracking(varr, 0 , n , k)
